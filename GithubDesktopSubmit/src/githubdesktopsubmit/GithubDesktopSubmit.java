293229515809fd62bdf00113e432bdf8b76a452e
St10441232 <166111199+St10441232@users.noreply.github.com>
2024-04-11 13:40:40 +0200

    Second Commit

---
+++ GithubDesktopSubmit/src/githubdesktopsubmit/GithubDesktopSubmit.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package githubdesktopsubmit;
+
+/**
+ *
+ * @author natzn
+ */
+public class GithubDesktopSubmit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Welcome to PROG5121 Tutorials");
+        System.out.println("Today we will be learning to ssubmit through github desktop");
+    }
+    
+}
---
+++ GithubDesktopSubmit/src/githubdesktopsubmit/NewClass.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package githubdesktopsubmit;
+
+/**
+ *
+ * @author natzn
+ */
+public class NewClass {
+    
+    public static void newMethod() {
+        System.out.println("This is our new class");
+    }
+}
+    
+
